76189e50dacb8e985ab1855a6bf87f0739ab9917
chinni00 <chinni0437@gmail>
2023-06-02 22:58:30 +0530

    meal summery added

---
+++ src/Components/Meals/MealsSummery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './MealsSummery.module.css'
+
+const MealsSummery = () => {
+  return (
+    <section className={classes.summery} >
+        <h2>Delicious Food , Delevered To You </h2> 
+        <p>Choose your fav meal from our board section of available meals
+            and enjoy a delicious lunch or dinner at home
+        </p>
+        <p>
+            All our meals are cooked with high-quality ingredients, Just-in-time and Of course by Experienced chef's
+        </p>
+    </section>
+  )
+}
+
+export default MealsSummery
