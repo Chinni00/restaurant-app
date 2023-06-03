8652b42e0ed86692e32971d2cf2224ef53bd1d22
chinni00 <chinni0437@gmail>
2023-06-03 23:36:57 +0530

    added props to button component on click to open cart items

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,12 +2,12 @@ import classes from './Header.module.css';
 import React from 'react';
 import HeaderContentButton from './HeaderContentButton';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <React.Fragment>
         <header className={classes.header}>
             <h1>DELICIOUS MEALS</h1>
-            <HeaderContentButton  /> 
+            <HeaderContentButton onClick={props.onShowCart} /> 
         </header>
         <div className={classes['main-image']}>
             <img src='https://i1.wp.com/mvslim.com/wp-content/uploads/2015/04/warsaw_food_restaurant_burger_kitchen_2.jpg?w=2130' alt='a table full of delicious food' />
