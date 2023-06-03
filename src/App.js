63f53537da0fb20d11e2f0d70a5703f1ef80149b
chinni00 <chinni0437@gmail>
2023-06-03 23:30:41 +0530

    sending props to app for displaying and hiding cart items in UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
-import React from 'react';
+import React ,{useState} from 'react';
 import './App.css';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandle=()=>{
+    setCartIsShown(true)
+  }
+
+  const hideCartHandle =()=>{
+    setCartIsShown(false)
+  }
+
   return (
     <React.Fragment>
-      <Header />
+
+      {cartIsShown && <Cart onClose={hideCartHandle} />}
+      <Header onShowCart={showCartHandle} />
       <main>
         <Meals />
       </main>
